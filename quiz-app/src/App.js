a29d73006f1a5074f70caa6ebe1e55390762e4a1
Madhukar kumar <madhukarkumar2468@gmail.com>
2024-03-20 20:30:52 +0530

    code

---
+++ quiz-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Quiz app</h1>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ quiz-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <Link to="/"></Link>
+    </div>
+  )
+}
+
+export default Header
